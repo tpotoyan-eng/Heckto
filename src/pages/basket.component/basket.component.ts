47b50bfa6b81d039ac6345ad0c539944dae224b8
Taron Potoyan <potoyantaron@gmail.com>
2025-12-29 15:15:50 +0400

    [INIT][init of header]

---
+++ src/pages/basket.component/basket.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basket.component',
+  imports: [],
+  templateUrl: './basket.component.html',
+  styleUrl: './basket.component.scss',
+})
+export class BasketComponent {
+
+}
